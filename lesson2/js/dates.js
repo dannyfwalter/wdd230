7e1d90cceaf3d6709a85725ce98c2b3dfafa005a
dannyfwalter <dfwalter@gmail.com>
2022-01-22 00:05:26 -0700

    lesson 2 modifications

---
+++ lesson2/js/dates.js
@@ -0,0 +1,5 @@
+const dateModified = new Date(document.lastModified);
+document.getElementById("modified").textContent = dateModified;
+
+const year = new Date(document.lastModified);
+document.getElementById("year").textContent = year.getFullYear;
