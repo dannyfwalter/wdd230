8bab6490176eb51107c2176cc02cec93665b9a44
dannyfwalter <dfwalter@gmail.com>
2022-03-21 19:44:24 -0600

    discover final changes.

--- lesson7/js/daysSinceVisit.js
+++ lesson7/js/daysSinceVisit.js
@@ -1,1 +1,20 @@
-console.log(Date.now);
+console.log(Date.now());
+
+const visit = document.querySelector("#visit");
+
+const today = Date.now();
+
+let last = Number(window.localStorage.getItem("LastVisited"));
+
+const difference = today - last;
+
+if (difference < 86400000) {
+    visit.textContent = "0";
+}
+else {
+    visit.textContent = Math.round(difference / 86400000);
+}
+
+console.log(difference);
+
+localStorage.setItem("LastVisited", today);
