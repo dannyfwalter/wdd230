1857ffbc323c7587514bf4435525a412fe96da33
dannyfwalter <dfwalter@gmail.com>
2022-04-06 02:02:02 -0600

    adding to the temple page and adding maps

--- temple/js/cards.js
+++ temple/js/cards.js
@@ -6,14 +6,22 @@ function displayTemples(temple) {
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
     let photo = document.createElement('img');
+    let address = document.createElement('p');
+    let phone = document.createElement('p');
 
     h2.textContent = `${temple.name}`;
     photo.setAttribute('src', temple.image);
     photo.setAttribute('alt', `Image of the ${temple.name} temple`);
     photo.setAttribute('loading', 'lazy');
 
+    address.textContent = `Address: ${temple.address}`;
+
+    phone.textContent = `Phone: ${temple.telephone}`;
+
     card.appendChild(h2);
     card.appendChild(photo);
+    card.appendChild(address);
+    card.appendChild(phone);
 
     cards.appendChild(card);
 }
